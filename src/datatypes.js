dfa795d7b7e77378b66d9fbcb95b497270846acf
vikash <vikashprogrammer123@gmail.com>
2026-04-28 11:49:50 +0530

    "28th-April"

---
+++ src/datatypes.js
@@ -0,0 +1,63 @@
+// data types: type of data
+
+//1. primitive data types: simple | stack | not part of object /reference | fixed memory | no GC
+
+//2 . non primitive data type:- object/Reference /class/Arrays/functions/Interfaces | dynamic memory | Heap
+
+
+//primitive data types:
+
+//1. number
+
+let i=10;
+console.log(i);
+console.log(typeof (i))
+
+
+
+let j=10.33;
+console.log(j);
+console.log(typeof (j))
+
+
+const PI=3.14;
+console.log(typeof(PI));
+
+//size
+ // 1 bytes=8bit
+ //size=8bytes=64 bits
+
+ //string : '', ""
+
+ let name="Testing";
+ let firstName="Naveen";
+ let gender='m';
+ let isExe='Y'
+ let ph='999129939';
+
+ console.log(typeof(firstName));
+ console.log(typeof gender)
+console.log(typeof isExe);
+console.log(typeof ph);
+
+let productName='MackBook Pro';
+console.log(productName);
+
+
+
+// size: 2 bytes per character
+let e='abc'; //=3*2=6 bytes
+console.log('my first name is '+firstName);
+
+
+
+//3. Boolean :- true/false
+let flag=true;
+console.log(typeof flag);
+let isEmpActive=true;
+console.log(isEmpActive);
+console.log(typeof flag);
+
+
+let rest='true';
+console.log(typeof rest);
