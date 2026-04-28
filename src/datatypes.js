ff8e0544cb2883184f5ecb70a3ba937d6a38d2b9
vikash <vikashprogrammer123@gmail.com>
2026-04-28 22:39:37 +0530

    chnages

--- src/datatypes.js
+++ src/datatypes.js
@@ -9,55 +9,55 @@
 
 //1. number
 
-let i=10;
+let i = 10;
 console.log(i);
 console.log(typeof (i))
 
 
 
-let j=10.33;
+let j = 10.33;
 console.log(j);
 console.log(typeof (j))
 
 
-const PI=3.14;
-console.log(typeof(PI));
+const PI = 3.14;
+console.log(typeof (PI));
 
 //size
- // 1 bytes=8bit
- //size=8bytes=64 bits
+// 1 bytes=8bit
+//size=8bytes=64 bits
 
- //string : '', ""
+//string : '', ""
 
- let name="Testing";
- let firstName="Naveen";
- let gender='m';
- let isExe='Y'
- let ph='999129939';
+let name = "Testing";
+let firstName = "Naveen";
+let gender = 'm';
+let isExe = 'Y'
+let ph = '999129939';
 
- console.log(typeof(firstName));
- console.log(typeof gender)
+console.log(typeof (firstName));
+console.log(typeof gender)
 console.log(typeof isExe);
 console.log(typeof ph);
 
-let productName='MackBook Pro';
+let productName = 'MackBook Pro';
 console.log(productName);
 
 
 
 // size: 2 bytes per character
-let e='abc'; //=3*2=6 bytes
-console.log('my first name is '+firstName);
+let e = 'abc'; //=3*2=6 bytes
+console.log('my first name is ' + firstName);
 
 
 
 //3. Boolean :- true/false
-let flag=true;
+let flag = true;
 console.log(typeof flag);
-let isEmpActive=true;
+let isEmpActive = true;
 console.log(isEmpActive);
 console.log(typeof flag);
 
 
-let rest='true';
+let rest = 'true';
 console.log(typeof rest);
