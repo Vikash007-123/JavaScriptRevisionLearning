19e090886b49c335f8799aeb7f753af3ec1c3373
vikash <vikashprogrammer123@gmail.com>
2026-04-28 18:16:09 +0530

    Lecture-3

---
+++ src/stringconcat.js
@@ -0,0 +1,24 @@
+//concat --merge -- add
+
+let a=10;
+let b=20;
+console.log(a+b);   //+->Addition
+
+
+console.log("Hello "+ "Playwright ");  
+
+let x='hello';
+let y='automation';
+
+console.log(a+b+x+y); //left->Right operstion
+console.log(x+y+a+b);
+
+
+console.log(x+y+(a+b));
+
+console.log(a+b+x+y+a+b); //30helloautomation1020
+console.log('the value of a: '+a);
+console.log('the value of a: '+b)
+console.log('the value of a: '+a+b)
+console.log('the value of a: '+(a+b));
+
